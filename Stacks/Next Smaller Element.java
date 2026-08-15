26531ef9b3d8cfcbd173e7e22060461e513daaa8
Disha-292005 <dishasirsikar29@gmail.com>
2026-08-15 23:19:29 +0530

    Implement Next Smaller Element algorithm

---
+++ Stacks/Next Smaller Element.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+
+    public static ArrayList<Integer> nextLargerElement(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                ans.add(-1);
+            } else {
+                ans.add(st.peek());
+            }
+
+            st.push(arr[i]);
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> result = nextLargerElement(arr);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}

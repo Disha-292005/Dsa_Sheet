996c7b2350171ffe88c26618b06bef4d6cd16eb6
Disha-292005 <dishasirsikar29@gmail.com>
2026-08-19 16:25:01 +0530

    Add maxValue method to find maximum in BST
    Implemented a method to find the maximum value in a Binary Search Tree (BST). The method traverses to the rightmost node to retrieve the maximum value.

---
+++ Trees and BST/Maximum Element in BST.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+
+class Solution {
+
+    public int maxValue(Node root) {
+
+        if (root == null) {
+            return -1;
+        }
+
+        while (root.right != null) {
+            root = root.right;
+        }
+
+        return root.data;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+                 8
+                / \
+               3   10
+              / \    \
+             1   6    14
+                / \
+               4   7
+        */
+
+        Node root = new Node(8);
+
+        root.left = new Node(3);
+        root.right = new Node(10);
+
+        root.left.left = new Node(1);
+        root.left.right = new Node(6);
+
+        root.left.right.left = new Node(4);
+        root.left.right.right = new Node(7);
+
+        root.right.right = new Node(14);
+
+        Solution obj = new Solution();
+
+        int result = obj.maxValue(root);
+
+        System.out.println("Maximum value = " + result);
+    }
+}

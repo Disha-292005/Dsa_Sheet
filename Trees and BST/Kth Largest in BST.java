f57e26dd61ff80a79e1bd3d4578653032e23f583
Disha-292005 <dishasirsikar29@gmail.com>
2026-08-20 11:17:31 +0530

    Create Kth Largest in BST.java

---
+++ Trees and BST/Kth Largest in BST.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+
+class Solution {
+
+    public int kthLargest(Node root, int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        return helper(root, k, ans);
+    }
+
+    int helper(Node root, int k, ArrayList<Integer> ans) {
+
+        if (root == null)
+            return -1;
+
+        // Visit right subtree first
+        int right = helper(root.right, k, ans);
+
+        if (right != -1)
+            return right;
+
+        // Visit root
+        ans.add(root.data);
+
+        if (ans.size() == k)
+            return ans.get(k - 1);
+
+        // Visit left subtree
+        return helper(root.left, k, ans);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+                  8
+                 / \
+                4   12
+               / \    \
+              2   6    14
+        */
+
+        Node root = new Node(8);
+
+        root.left = new Node(4);
+        root.right = new Node(12);
+
+        root.left.left = new Node(2);
+        root.left.right = new Node(6);
+
+        root.right.right = new Node(14);
+
+        Solution obj = new Solution();
+
+        int k = 3;
+
+        int result = obj.kthLargest(root, k);
+
+        System.out.println("Kth largest element = " + result);
+    }
+}
